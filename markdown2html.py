c83878d443923251615e7b1c5e1b21e95a5607e6
JaiberS <758@holbertonschool.com>
2020-12-18 12:26:38 -0500

    replace # with headings

--- markdown2html.py
+++ markdown2html.py
@@ -11,9 +11,22 @@ if __name__ == "__main__":
     error = None
     if len(sys.argv) < 2:
         error = 'Usage: ./markdown2html.py README.md README.html'
-    elif not path.exists(sys.argv[1]):
+    elif not path.exists(sys.argv[1]) or not path.isfile(sys.argv[1]):
         error = 'Missing ' + sys.argv[1]
     if error is not None:
         sys.stderr.write(error + '\n')
         exit(1)
+
+    Markdown = open(sys.argv[1])
+    linesList = Markdown.readlines()
+    for i in range(len(linesList)):
+        counter = linesList[i].count('#')
+        linesList[i] = linesList[i].replace('#', '')
+        linesList[i] = linesList[i].replace('\n', '')
+        linesList[i] = '<h' + str(counter) + '>' + linesList[i][1:] + '</h' + str(counter) + '>\n'
+    linesAgain = ''.join(linesList)
+    outputFile = open(sys.argv[2], 'w')
+    outputFile.write(linesAgain)
+    Markdown.close()
+    outputFile.close()
     exit(0)
