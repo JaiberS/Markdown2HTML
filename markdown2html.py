0af917577b098bae96d3b2419eea71f375d678fa
JaiberS <758@holbertonschool.com>
2020-12-18 13:13:30 -0500

    ordered list, prev was unordered

--- markdown2html.py
+++ markdown2html.py
@@ -19,6 +19,7 @@ if __name__ == "__main__":
 
     Markdown = open(sys.argv[1])
     linesList = Markdown.readlines()
+    ul = False
     ol = False
     for i in range(len(linesList)):
         if linesList[i][0] == '#':
@@ -30,14 +31,26 @@ if __name__ == "__main__":
             linesList[i] = linesList[i].replace('-', '')
             linesList[i] = linesList[i].replace('\n', '')
             linesList[i] = '<li>' + linesList[i][1:] + '</li>\n'
-            if not ol:
-                ol = True
+            if not ul:
+                ul = True
                 linesList[i] = '<ul>\n' + linesList[i]
             if linesList[i] == linesList[-1]:
                 linesList[i] = linesList[i] + '</ul>\n'
+        elif ul == True:
+            ul = False
+            linesList[i - 1] = linesList[i - 1] + '</ul>\n'
+        if linesList[i][0] == '*':
+            linesList[i] = linesList[i].replace('*', '')
+            linesList[i] = linesList[i].replace('\n', '')
+            linesList[i] = '<li>' + linesList[i][1:] + '</li>\n'
+            if not ol:
+                ol = True
+                linesList[i] = '<ol>\n' + linesList[i]
+            if linesList[i] == linesList[-1]:
+                linesList[i] = linesList[i] + '</ol>\n'
         elif ol == True:
             ol = False
-            linesList[i] = '</ul>\n' + linesList[i]
+            linesList[i - 1] =  linesList[i - 1] + '</ol>\n'
     linesAgain = ''.join(linesList)
     outputFile = open(sys.argv[2], 'w')
     outputFile.write(linesAgain)
